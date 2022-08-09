8d4d07dbe5a0429f673dcbd1ff08826c939797a6
netindo <anashrulyusuf@gmail.com>
2022-08-09 10:25:51 +0700

    crud

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -24,21 +24,7 @@
                 <th>Action</th>
             </tr>
             </thead>
-            <tbody  id="users-list">
-            <?php if($users): ?>
-                <?php foreach($users as $user): ?>
-                    <tr>
-                        <td><?php echo $user['id']; ?></td>
-                        <td><?php echo $user['name']; ?></td>
-                        <td><?php echo $user['email']; ?></td>
-                        <td>
-                            <a href="<?php echo base_url('edit-view/'.$user['id']);?>" class="btn btn-primary btn-sm">Edit</a>
-                            <a href="<?php echo base_url('delete/'.$user['id']);?>" class="btn btn-danger btn-sm">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
-            </tbody>
+            <tbody  id="users-list"></tbody>
         </table>
     </div>
 </div>
